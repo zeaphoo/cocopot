0131f1f46762f57aae6f6cc0d55dcc7d6f0ab8dc
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-08-25 14:15:08 +0800

    remove unused filemultidict

--- flagon/datastructures.py
+++ flagon/datastructures.py
@@ -237,34 +237,6 @@ class HeaderDict(MultiDict):
                 del self.dict[name]
 
 
-class FileMultiDict(MultiDict):
-    """A special :class:`MultiDict` that has convenience methods to add
-    files to it.
-    """
-
-    def add_file(self, name, file, filename=None, content_type=None):
-        """Adds a new file to the dict.  `file` can be a file name or
-        a :class:`file`-like or a :class:`FileStorage` object.
-
-        :param name: the name of the field.
-        :param file: a filename or :class:`file`-like object
-        :param filename: an optional filename
-        :param content_type: an optional content type
-        """
-        if isinstance(file, FileStorage):
-            value = file
-        else:
-            if isinstance(file, string_types):
-                if filename is None:
-                    filename = file
-                file = open(file, 'rb')
-            if filename and content_type is None:
-                content_type = mimetypes.guess_type(filename)[0] or \
-                               'application/octet-stream'
-            value = FileStorage(file, filename, name, content_type)
-
-        self.add(name, value)
-
 class WSGIHeaders(DictMixin):
     """ This dict-like class wraps a WSGI environ dict and provides convenient
         access to HTTP_* fields. Keys and values are native strings
