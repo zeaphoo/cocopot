f3d08e0e38d81feb686db8c59c7bc4ad3392f4c0
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-09-16 20:57:29 +0800

    more testcase in test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@ import pytest
 
 from flagon import Flagon, Blueprint, request, g
 import copy
+import traceback
 
 env1 = {
     'REQUEST_METHOD':       'GET',
@@ -18,16 +19,18 @@ env1 = {
 }
 
 def start_response(x, y):
-    pass
+    print x, y
 
 def test_basic_app():
     app = Flagon()
     @app.before_request
     def before_request1():
+        print 'before_request'
         g.a = 1
 
     @app.before_request
     def before_request2():
+        print 'before_request2'
         g.b = 2
 
     @app.route('/hello')
@@ -36,14 +39,20 @@ def test_basic_app():
 
     @app.after_request
     def after_request1(resp):
-        assert g.a == 1
-        assert g.b == 2
         return resp
 
     @app.after_request
     def after_request2(resp):
         return resp
 
+    @app.errorhandler(404)
+    def error_handler404(exception):
+        return '404'
+
+    @app.errorhandler(500)
+    def error_handler500(exception):
+        print traceback.format_exc()
+        return '500'
 
     assert app.before_request_funcs[None][0] == before_request1
     assert app.before_request_funcs[None][1] == before_request2
@@ -63,6 +72,10 @@ def test_basic_app():
     def bp_app_before_request():
         pass
 
+    @bp.errorhandler(Exception)
+    def bp_errorhandler(exception):
+        pass
+
     app.register_blueprint(bp)
     assert app.before_request_funcs[None][2] == bp_app_before_request
     assert app.before_request_funcs['foo'][0] == bp_before_request
@@ -73,3 +86,6 @@ def test_basic_app():
     env = copy.deepcopy(env1)
     env['PATH_INFO'] = '/foo/bar'
     assert app(env, start_response)  == 'bar'
+    env = copy.deepcopy(env1)
+    env['PATH_INFO'] = '/foo2'
+    assert app(env, start_response)  == '404'
