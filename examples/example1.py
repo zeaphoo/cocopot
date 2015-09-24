583ea246e6cf06c466eec8a9432ff8c7642ca371
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-09-24 18:14:28 +0800

    more complex example for view_args

--- examples/example1.py
+++ examples/example1.py
@@ -3,13 +3,22 @@ app = Flagon('example1')
 
 @app.before_request
 def before_request():
-    if 'foo' not in request.view_args:
-        request.view_args.update(foo='123')
+    if request.endpoint == 'hello_world':
+        if 'foo' not in request.view_args:
+            request.view_args.update(foo='123')
+    if request.endpoint == 'hello_world2':
+        if 'foo2' in request.view_args:
+            request.view_args.pop('foo2')
 
 @app.route('/')
 @app.route('/<foo>/bar')
 def hello_world(foo):
     print 'hello', foo
+    return 'Hello World!'
+
+@app.route('/')
+@app.route('/<foo2>/noarg')
+def hello_world2():
     return 'Hello World!'
 
 if __name__ == '__main__':
