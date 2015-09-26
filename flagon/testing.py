9e368d0dc8a9b824f46a8537d8b10261fd1be8b5
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-09-26 20:25:40 +0800

    add testing module

---
+++ flagon/testing.py
@@ -0,0 +1,17 @@
+
+
+class EnvironBuilder(object):
+    pass
+
+
+class FlagonClinet(object):
+    """
+    """
+
+    preserve_context = False
+
+    def open(self, *args, **kwargs):
+        as_tuple = kwargs.pop('as_tuple', False)
+        buffered = kwargs.pop('buffered', False)
+        follow_redirects = kwargs.pop('follow_redirects', False)
+        builder = make_test_environ_builder(self.application, *args, **kwargs)
