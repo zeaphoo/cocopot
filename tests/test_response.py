299e68994b4d733332b1ddc4b1cd03305c04a763
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-10-15 14:00:22 +0800

    add response testcase

--- tests/test_response.py
+++ tests/test_response.py
@@ -8,14 +8,16 @@ import copy
 def test_basic_response():
     r = make_response('text')
     assert r.body == 'text'
-    assert r._status_line == '200 OK'
+    assert r.status_line == '200 OK'
+    assert r.status_code == 200
     r = make_response('redirect', 302)
-    assert r._status_line == '302 Found'
+    assert r.status_line == '302 Found'
+    assert r.status_code == 302
 
 def test_redirect():
     r = redirect('http://example.com/foo/new', 302)
-    assert r._status_line == '302 Found'
+    assert r.status_line == '302 Found'
     assert r.headers['location'] == 'http://example.com/foo/new'
     r = redirect('http://example.com/foo/new2', 301)
-    assert r._status_line == '301 Moved Permanently'
+    assert r.status_line == '301 Moved Permanently'
     assert r.headers['location'] == 'http://example.com/foo/new2'
