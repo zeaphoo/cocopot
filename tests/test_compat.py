7cfadb37f51299921a16c4e10206a8b6633b2e72
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-10-27 11:49:39 +0800

    add compat testcase

---
+++ tests/test_compat.py
@@ -0,0 +1,23 @@
+import pytest
+
+from flagon._compat import *
+import copy
+import traceback
+import sys
+
+def test_compat():
+    assert to_bytes(None) == None
+    assert to_unicode(None) == None
+    with pytest.raises(ZeroDivisionError):
+        try:
+            s = 1/0
+        except:
+            exc_type, exc_value, tb = sys.exc_info()
+            reraise(exc_type, exc_value, tb)
+
+    with pytest.raises(ZeroDivisionError):
+        try:
+            s = 1/0
+        except:
+            exc_type, exc_value, tb = sys.exc_info()
+            reraise(exc_type, exc_value, None)
