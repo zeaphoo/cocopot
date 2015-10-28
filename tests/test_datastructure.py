563747cf8d3623be1c5c83dfbcfe481e986606f4
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-10-28 12:09:56 +0800

    add mutliform testcase

--- tests/test_datastructure.py
+++ tests/test_datastructure.py
@@ -7,3 +7,17 @@ def test_basic_multidict():
     assert d['a'] == 'b'
     assert d.getlist('a') == ['b', 'c']
     assert ('a' in d) == True
+
+    d = MultiDict([('a', 'b'), ('a', 'c')], a='dddd')
+    assert d['a'] == 'b'
+    assert d.getlist('a') == ['b', 'c', 'dddd']
+    assert len(d) == 1
+    assert list(d.keys()) == ['a']
+    assert list(d.values()) == ['b']
+    d.replace('a', 'ee')
+    assert d['a'] == 'ee'
+    assert d.getlist('a') == ['ee']
+    assert d.get('foo') == None
+
+    del d['a']
+    assert len(d) == 0
